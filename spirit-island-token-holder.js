d40c3b40bd99e56abc8033b2582b038b75854487
Cameron Spear <cameronspear@gmail.com>
2020-03-11 08:39:32 -0700

    Lid stuff

--- spirit-island-token-holder.js
+++ spirit-island-token-holder.js
@@ -1,13 +1,20 @@
 const GAME_BOX_WIDTH = 288;
 const SPIRIT_CARD_HEIGHT = 153;
-const BORDER_WIDTH = 1;
-const DEPTH = 40;
 const ROW_COUNT = 12;
 const COL_COUNT = 3;
 const RAMP_SIZE = 8;
 
 
 const WIGGLE_ROOM = 5;
+const BORDER_WIDTH = 1;
+const THICKNESS = BORDER_WIDTH * 3;
+const LIPNESS = BORDER_WIDTH * 2;
+const EXTRA = 0.2;
+const DEPTH = 40 - LIPNESS - EXTRA;
+const LIP_HANG_ON_NESS = 1;
+
+const BUTTON_RADIUS = 7;
+const BUTTON_DISTANCE_FROM_EDGE = 15;
 
 const PRINTER_MAX = 240;
 // END CONFIG
@@ -18,12 +25,14 @@ const BOX_HEIGHT = GAME_BOX_WIDTH - SPIRIT_CARD_HEIGHT;
 const BOX_HEIGHT_WITH_WIGGLE = BOX_HEIGHT - WIGGLE_ROOM;
 const BOX_WIDTH_WITH_WIGGLE = Math.min(GAME_BOX_WIDTH - WIGGLE_ROOM, PRINTER_MAX);
 
-const CELL_HEIGHT = ((BOX_HEIGHT_WITH_WIGGLE - BORDER_WIDTH) / COL_COUNT) - BORDER_WIDTH;
-const ROW_UNIT_WIDTH = ((BOX_WIDTH_WITH_WIGGLE - BORDER_WIDTH) / ROW_COUNT) - BORDER_WIDTH;
+const CELL_HEIGHT = ((BOX_HEIGHT_WITH_WIGGLE - BORDER_WIDTH - BORDER_WIDTH - THICKNESS) / COL_COUNT) - BORDER_WIDTH;
+const ROW_UNIT_WIDTH = ((BOX_WIDTH_WITH_WIGGLE - BORDER_WIDTH - BORDER_WIDTH - THICKNESS) / ROW_COUNT) - BORDER_WIDTH;
 
 function main() {
   console.log(ROW_UNIT_WIDTH, CELL_HEIGHT);
   console.log(BOX_WIDTH_WITH_WIGGLE, BOX_HEIGHT_WITH_WIGGLE);
+
+  return lid();
 
   const grid = [
     [5, 2, 2, 3],
@@ -45,7 +54,10 @@ function main() {
   );
 
   return difference(
-    cube({ size: [BOX_WIDTH_WITH_WIGGLE, BOX_HEIGHT_WITH_WIGGLE, DEPTH] }),
+    union(
+      cube({ size: [BOX_WIDTH_WITH_WIGGLE, BOX_HEIGHT_WITH_WIGGLE, DEPTH] }),
+      translate([0, 0, DEPTH], makeLidLip())
+    ),
     negative
   );
 }
@@ -55,8 +67,8 @@ function makeHole(x, y, size) {
 
   return translate(
     [
-      BORDER_WIDTH + (x * (ROW_UNIT_WIDTH + BORDER_WIDTH)),
-      BORDER_WIDTH + (y * (CELL_HEIGHT + BORDER_WIDTH)),
+      THICKNESS + (x * (ROW_UNIT_WIDTH + BORDER_WIDTH)),
+      THICKNESS + (y * (CELL_HEIGHT + BORDER_WIDTH)),
       BORDER_WIDTH
     ],
     difference(
@@ -80,6 +92,67 @@ function makeRamp(l) {
         cube({ size: [l, RAMP_SIZE, RAMP_SIZE] }),
         rotate([0, 90, 0], cylinder({ r: RAMP_SIZE, h: l, fn: 64 }))
       )
+    )
+  );
+}
+
+
+function makeLidLip() {
+  // const squareLip = difference(
+  //   cube({ size: [BOX_WIDTH_WITH_WIGGLE, BOX_HEIGHT_WITH_WIGGLE, THICKNESS + EXTRA] }),
+  //   translate(
+  //     [0, THICKNESS, 0],
+  //     cube({ size: [BOX_WIDTH_WITH_WIGGLE - (THICKNESS), BOX_HEIGHT_WITH_WIGGLE - (THICKNESS * 2), THICKNESS + EXTRA] })
+  //   )
+  // );
+
+  const xLeg = THICKNESS - LIP_HANG_ON_NESS;
+  const yLeg = LIPNESS;
+
+  return union(
+    translate([0, BOX_HEIGHT_WITH_WIGGLE - THICKNESS, 0], makeLipPart(BOX_WIDTH_WITH_WIGGLE)),
+    translate([0, THICKNESS, 0], mirror([0, 1, 0], makeLipPart(BOX_WIDTH_WITH_WIGGLE))),
+    translate([BOX_WIDTH_WITH_WIGGLE - THICKNESS, BOX_HEIGHT_WITH_WIGGLE, 0], rotate([0, 0, -90], makeLipPart(BOX_HEIGHT_WITH_WIGGLE)))
+  );
+
+  function makeLipPart(len) {
+
+    const squareLip = cube({
+      size: [len, THICKNESS, LIPNESS + EXTRA]
+    });
+
+    const cutout = polyhedron({
+      points: [
+        [0, 0, 0], [0, xLeg, 0], [0, 0, yLeg],
+        [len, 0, 0], [len, xLeg, 0], [len, 0, yLeg]
+      ],
+      triangles: [
+        [2, 0, 1],
+        [3, 5, 4],
+        [5, 0, 2],
+        [5, 3, 0],
+        [4, 0, 3],
+        [4, 1, 0],
+        [5, 1, 4],
+        [5, 2, 1]
+      ]
+    });
+
+    return difference(
+      squareLip,
+      translate([0, 0, EXTRA], cutout),
+      translate([0, -1 * LIP_HANG_ON_NESS, -1 * (LIPNESS)], squareLip)
+    );
+  }
+}
+
+function lid() {
+  return translate(
+    [0, 0, -1 * EXTRA],
+    difference(
+      translate([0, 0, EXTRA], cube({ size: [BOX_WIDTH_WITH_WIGGLE, BOX_HEIGHT_WITH_WIGGLE, LIPNESS] })),
+      makeLidLip(),
+      translate([BUTTON_DISTANCE_FROM_EDGE, (BOX_HEIGHT_WITH_WIGGLE / 2), 0], cylinder({ r: BUTTON_RADIUS, h: LIPNESS * .6, fn: 64 }))
     )
   );
 }
