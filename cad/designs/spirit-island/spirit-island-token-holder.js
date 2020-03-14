28e82cf507ace6948ec965741e1604fb3dff1c55
Cameron Spear <cameronspear@gmail.com>
2020-03-14 12:00:20 -0700

    Update some designs

--- cad/designs/spirit-island/spirit-island-token-holder.js
+++ cad/designs/spirit-island/spirit-island-token-holder.js
@@ -1,13 +1,14 @@
-const CONTAINER_WIDTH = 120;
-const CONTAINER_LENGTH = 120;
+const CONTAINER_WIDTH = inToMm(9);
+const CONTAINER_LENGTH = inToMm(6);
 const CONTAINER_HEIGHT = 30;
 
+const MAKE_LID = false;
 
 const GRID = [
-  [2, 2],
-  [2, 2]
+  [1]
 ];
 
+const USE_RAMPS = false;
 
 // minor config, unlikely to change
 const RAMP_SIZE = 8;
@@ -17,8 +18,11 @@ const LID_HEIGHT = 2;
 const BUTTON_RADIUS = 6;
 const BUTTON_DISTANCE_FROM_EDGE = 15;
 
+const SIDE_HOLE = true;
+
 // better name
-const EXTRA = 0.2;
+const EXTRA_LIP_HEIGHT = 0.2;
+const EXTRA_LIP_WIGGLE_ROOM = 0;
 
 // END CONFIG //
 
@@ -33,7 +37,17 @@ const CELL_HEIGHT = ((CONTAINER_LENGTH - BORDER_WIDTH - BORDER_WIDTH - CONTAINER
 function main() {
   console.log(CONTAINER_WIDTH, CONTAINER_LENGTH);
 
-  // return lid();
+  if (CONTAINER_WIDTH > PRINTER_MAX_WIDTH) {
+    throw new Error(`Container is too big! Max width is: ${PRINTER_MAX_WIDTH}. Width provided: ${CONTAINER_WIDTH}`);
+  }
+
+  if (CONTAINER_LENGTH > PRINTER_MAX_LENGTH) {
+    throw new Error(`Container is too big! Max length is: ${PRINTER_MAX_LENGTH}. Length provided: ${CONTAINER_LENGTH}`);
+  }
+
+  if (MAKE_LID) {
+    return lid();
+  }
 
   const holes = [];
 
@@ -48,8 +62,10 @@ function main() {
     });
   });
 
+  const sideHoleSize = 20;
   const negative = union(
-    ...holes
+    ...holes,
+    ...(SIDE_HOLE ? [translate([0, (CONTAINER_LENGTH / 2) - (sideHoleSize / 2)], makeSideHole(sideHoleSize))] : [])
   );
 
   return difference(
@@ -74,12 +90,18 @@ function makeHole(x, y, size, rowUnitWidth) {
       cube({
         size: [width, CELL_HEIGHT, CONTAINER_HEIGHT]
       }),
-      translate([RAMP_SIZE, 0, 0], rotate([0, 0, 90], makeRamp(CELL_HEIGHT))),
-      translate([width, RAMP_SIZE, 0], rotate([0, 0, 180], makeRamp(width))),
-      translate([width - (RAMP_SIZE), CELL_HEIGHT, 0], rotate([0, 0, 270], makeRamp(CELL_HEIGHT))),
-      translate([0, CELL_HEIGHT - (RAMP_SIZE), 0], rotate([0, 0, 0], makeRamp(width)))
+      ...(USE_RAMPS ? addRamps(width) : [])
     )
   );
+}
+
+function addRamps(width) {
+  return [
+    translate([RAMP_SIZE, 0, 0], rotate([0, 0, 90], makeRamp(CELL_HEIGHT))),
+    translate([width, RAMP_SIZE, 0], rotate([0, 0, 180], makeRamp(width))),
+    translate([width - (RAMP_SIZE), CELL_HEIGHT, 0], rotate([0, 0, 270], makeRamp(CELL_HEIGHT))),
+    translate([0, CELL_HEIGHT - (RAMP_SIZE), 0], rotate([0, 0, 0], makeRamp(width)))
+  ];
 }
 
 function makeRamp(l) {
@@ -95,6 +117,11 @@ function makeRamp(l) {
   );
 }
 
+function makeSideHole(size) {
+  return cube({
+    size: [size / 2, size, CONTAINER_HEIGHT]
+  });
+}
 
 function makeLidLip(offset = 0) {
   const xLeg = CONTAINER_EDGE_WIDTH - LIP_HANG_ON_NESS;
@@ -115,7 +142,7 @@ function makeLidLip(offset = 0) {
   function makeLipPart(len) {
 
     const squareLip = cube({
-      size: [len, CONTAINER_EDGE_WIDTH, LID_HEIGHT + EXTRA]
+      size: [len, CONTAINER_EDGE_WIDTH, LID_HEIGHT + EXTRA_LIP_HEIGHT]
     });
 
     const cutout = polyhedron({
@@ -137,20 +164,27 @@ function makeLidLip(offset = 0) {
 
     return difference(
       squareLip,
-      translate([0, 0, EXTRA], cutout),
+      translate([0, 0, EXTRA_LIP_HEIGHT], cutout),
       translate([0, -1 * LIP_HANG_ON_NESS, -1 * (LID_HEIGHT)], squareLip)
     );
   }
 }
 
 function lid() {
-  const offset = 0.1;
-
   return translate(
-    [0, 0, -1 * EXTRA],
+    [0, 0, -1 * EXTRA_LIP_HEIGHT],
     difference(
-      translate([0, 0, EXTRA], cube({ size: [CONTAINER_WIDTH - offset, CONTAINER_LENGTH - offset - offset, LID_HEIGHT] })),
-      makeLidLip(offset),
+      translate(
+        [0, 0, EXTRA_LIP_HEIGHT],
+        cube({
+          size: [
+            CONTAINER_WIDTH - EXTRA_LIP_WIGGLE_ROOM,
+            CONTAINER_LENGTH - EXTRA_LIP_WIGGLE_ROOM - EXTRA_LIP_WIGGLE_ROOM,
+            LID_HEIGHT
+          ]
+        })
+      ),
+      makeLidLip(EXTRA_LIP_WIGGLE_ROOM),
       translate(
         [BUTTON_DISTANCE_FROM_EDGE, (CONTAINER_LENGTH / 2) - BUTTON_RADIUS - 3, LID_HEIGHT - (LID_HEIGHT * 0.6)],
         cylinder({ r: BUTTON_RADIUS, h: LID_HEIGHT, fn: 64 })
@@ -161,4 +195,8 @@ function lid() {
       )
     )
   );
+}
+
+function inToMm(inches) {
+  return inches * 25.4;
 }
